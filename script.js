0914db6abb0045fdc13324bfd41b53e323f6aa2b
Frerik Nagel <57255358+Thepenguincoder@users.noreply.github.com>
2021-09-15 10:01:18 +0000

    Birb aangemaakt

--- script.js
+++ script.js
@@ -1,60 +1,52 @@
-function setup() {
-	createCanvas(500, 400);
+class Birb{
+
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+  }
+
+  draw() {
+    fill(this.c);
+    //noStroke();
+    ellipse(this.x, this.y, this.w, this.h)
+  
+    //versnellen
+    if(keyIsDown(32)){
+
+    } 
+    
+    if (this.y > height - (this.h / 2)|| this.y < (this.h/2)) {
+      if (this.y > height - (this.h/2)) {
+       this.y = height - (this.h/2);
+      } else if ( this.y < (this.h/2)){
+        this.y = (this.h/2);
+      }
+    }
+
+  
+
+
+  }
 }
 
-var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+
+var birb
+
+
+function setup() {
+  createCanvas(500, 500);
+
+  birb = new Birb(50, 50, 20, 20, "yellow")
+
+}
 
 function draw() {
-	background(225);
-	
-	fill(0);
-	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
-	
-	fill(0, 255, 0);
-	rect(xpos, ypos, 50, 50);
-	
-	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
-	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
+	background(225);  
+  
+  birb.draw();
+
 }
 
-function keyPressed() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = -2;
-			break;
-		case 39:
-		case 68:
-			xspeed = 2;
-			break;
-		case 38:
-		case 87:
-			yspeed = -2;
-			break;
-		case 40:
-		case 83:
-			yspeed = 2;
-			break;
-	}
-}
-
-function keyReleased() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = 0;
-			break;
-		case 39:
-		case 68:
-			xspeed = 0;
-			break;
-		case 38:
-		case 87:
-			yspeed = 0;
-			break;
-		case 40:
-		case 83:
-			yspeed = 0;
-			break;
-	}
-}
